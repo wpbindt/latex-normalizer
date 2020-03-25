6cb1d02e868e3eefbfbb3a79e7785b8686308f65
wpbindt <wesselbindt@gmail.com>
2020-03-25 19:10:47 +0100

    Made some docstrings PEP257 compliant

--- latex_normalizer.py
+++ latex_normalizer.py
@@ -4,7 +4,13 @@ import re
 
 def _remove_line_comments(text):
     '''
-    Removes line comments, but not percent symbols.
+    Remove line comments from latex code.
+    
+    Specifically, this takes all text between % and the next newline,
+    and replaces it with a single space. This is not done if the percent
+    symbol is preceded by an odd number of back slashes. This is because
+    for example "\\\% " is not ignored by the latex compiler. It prints
+    a percent symbol on a new line.
     '''
     line_comment_regex = re.compile(r'(?<!\\)(?:\\\\)*%.*\n')
     return line_comment_regex.sub(' ', text)
@@ -12,10 +18,11 @@ def _remove_line_comments(text):
 
 def _remove_accents(text):
     '''
-    Replaces letters with diacritics by their non-
-    diacritical counterpart. For example,
-    hyperk\"ahler becomes hyperkahler. Moreover,
-    hyperk\"{a}hler becomes hyperkahler too.
+    Replace latex code for diacritics with the empty string.
+
+    Replace letters with diacritics by their non-diacritical
+    counterpart. For example, hyperk\"ahler becomes hyperkahler. 
+    Moreover, hyperk\"{a}hler becomes hyperkahler too.
     '''
     letters = [
             'u',
@@ -53,9 +60,22 @@ def _remove_accents(text):
 
 def _normalize_commands(text):
     '''
-    Replaces "\command{bla}" with " bla ". Useful
-    for things like \emph, and \text. Adapted from
-    remove_command from arxiv-latex-cleaner.
+    Replace a list of specified latex commands with their arguments.
+
+    More concretely, this function replaces "\command{argument}" with
+    " argument ", where command is a command from the list
+        subsubsection,
+        subsection,
+        section,
+        chapter,
+        title,
+        author,
+        footnote,
+        emph,
+        text,
+        textit,
+        textrm.
+    This is adapted from remove_command, from arxiv-latex-cleaner.
     '''
     normalized_commands = [
         'subsubsection',
@@ -81,10 +101,18 @@ def _normalize_commands(text):
 
 def _remove_environments(text):
     '''
-    Removes environments and their content,
-    for a specified list of environments.
-    Specifically used for multiline comments,
-    figures, and display equations.
+    Remove a specified list of latex environments.
+
+    Specifically, replace "\begin{environment} contents 
+    \end{environment}" with the empty string, where environment is
+    an environment on the list
+        comment,
+        figure,
+        tikzpicture,
+        equation(*),
+        multline(*),
+        align(*),
+        gather(*).
     '''
     removed_env = [
         r'comment',
@@ -107,7 +135,7 @@ def _remove_environments(text):
 
 def _strip_environments_labels(text):
     '''
-    Removes environment delimiters and labels.
+    Remove environment delimiters and labels.
     '''
     env_regex = re.compile(r'\\(begin|end|label){.*?}')
     return env_regex.sub(' ', text)
@@ -115,7 +143,11 @@ def _strip_environments_labels(text):
 
 def _remove_commands(text):
     '''
-    Removes '\bla{di}[bla]...{bla}' entirely.
+    Remove entire commands.
+
+    More concretely, this remove all commands and their arguments
+    (optional or otherwise). That is, "\a*{b}[c]{d}$ is replaced with
+    a single space.
     '''
     command_regex = re.compile(r'''
                         \\\w*(\*)?
@@ -128,9 +160,12 @@ def _remove_commands(text):
 
 def _remove_equations(text):
     '''
-    Removes inline equations delimited
-    by $ and \(, and removes display 
-    equations delimited by $$ and \[.
+    Remove inline and display equations.
+
+    Removes inline equations delimited by $ and \(, and removes display
+    equations delimited by $$ and \[. Ignores dollar signs preceded by
+    a single backslash, as the latex compiler renders that as a dollar
+    sign.
     '''
     eqn_regex = re.compile(r'''
                     ((?<!\\)\${1,2}.*?(?<!\\)\${1,2})
@@ -143,8 +178,7 @@ def _remove_equations(text):
 
 def _remove_special_characters(text):
     '''
-    Replaces anything that is not in
-    the alphabet by a single space.
+    Replace characters that are not letters or whitespaces by a space.
     '''
     non_alphabet_regex = re.compile(r'[^a-zA-Z\s]')
     return non_alphabet_regex.sub(' ', text)
@@ -152,16 +186,14 @@ def _remove_special_characters(text):
 
 def _remove_white_space(text):
     '''
-    Removes all spaces, tabs, and new
-    lines, and replaces them with a 
-    single space.
+    Replace white space (including tabs and newlines) by a single space.
     '''
     return " ".join(text.split())
 
 
 def latex_normalizer(text):
     '''
-    Takes a string containing latex syntax, 
+    Take a string containing latex syntax, 
     and returns a string stripped of that 
     syntax. For example,
     "\begin{document} Hi! \end{document}"
